84d8b476b51341fa8ea75302b8498f1f6585b802
Derick Hermanson <derick.hermanson@gmail.com>
2024-12-23 15:33:26 -0600

    add serde example with avro json

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ This repository is a place to learn how to use [avro4k](https://github.com/avro-
 ### Sealed Interface Support
 
 See [this test class](https://github.com/avro-kotlin/avro4k/blob/main/src/test/kotlin/com/github/avrokotlin/avro4k/schema/UnionSchemaTest.kt) for an example of how to use sealed classes and interfaces.
+
+### Article from kotlin project
+
+See [this article](https://kotlinlang.org/api/kotlinx.serialization/kotlinx-serialization-core/kotlinx.serialization/-sealed-class-serializer/)
--- lib/src/test/kotlin/org/example/TinkeringTests.kt
+++ lib/src/test/kotlin/org/example/TinkeringTests.kt
@@ -1,12 +1,19 @@
 package org.example
 
 import com.github.avrokotlin.avro4k.Avro
+import com.github.avrokotlin.avro4k.decodeFromGenericData
+import com.github.avrokotlin.avro4k.encodeToGenericData
 import com.github.avrokotlin.avro4k.schema
 import kotlinx.serialization.*
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
 import kotlinx.serialization.modules.subclass
 import org.apache.avro.SchemaNormalization
+import org.apache.avro.generic.GenericDatumReader
+import org.apache.avro.generic.GenericDatumWriter
+import org.apache.avro.io.DecoderFactory
+import org.apache.avro.io.EncoderFactory
+import org.apache.commons.io.output.ByteArrayOutputStream
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -48,6 +55,40 @@ class TinkeringTests {
         val roundTripEvent = avro.decodeFromByteArray(writerSchema, serializer, serializedEvent)
 
         assertEquals(roundTripEvent, initialEvent)
+    }
+
+    @Test
+    fun itShouldEncodeAndDecodeWithJson() {
+        val avro = Avro {
+            this.validateSerialization = true
+        }
+        val (module, serializer) = accountEventSerializerAndModule()
+        val writerSchema = avro.schema(serializer)
+        val fingerprint = SchemaNormalization.parsingFingerprint64(writerSchema)
+
+        val initialEvent = AccountEvent.Opened(100u)
+
+        val genericInitialEvent = avro.encodeToGenericData(writerSchema, initialEvent)
+
+        val out = ByteArrayOutputStream()
+        val writer = GenericDatumWriter<Any>(writerSchema)
+        val encoder = EncoderFactory.get().jsonEncoder(writerSchema, out)
+        writer.write(genericInitialEvent, encoder)
+        encoder.flush()
+        out.close()
+
+        val jsonOutput = out.toString(Charsets.UTF_8)
+        println(jsonOutput)
+
+
+        val reader = GenericDatumReader<Any>(writerSchema)
+        val decoder = DecoderFactory.get().jsonDecoder(writerSchema, jsonOutput)
+        val genericRoundTripEvent = reader.read(null, decoder)
+
+        val roundTripInitialEvent = avro.decodeFromGenericData(writerSchema, serializer, genericRoundTripEvent)
+
+        assertEquals(roundTripInitialEvent, initialEvent)
+
     }
 
     fun accountEventSerializerAndModule() : Pair<SerializersModule, KSerializer<AccountEvent>> {
